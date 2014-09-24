6353b744db00fde42b26304834a77ba84e67c87b
Alessandro Berbenni <aberbenni@Mac-mini-di-Alessandro.local>
2014-09-24 12:14:18 +0200

    Fixed append plugin config comment

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -12,29 +12,35 @@ eventCompileStart = { kind ->
 	def newConfigFiles = ["classpath:AngularScaffoldingConfig.groovy"]
 	event("StatusUpdate", ["Adding $newConfigFiles configuration files to grails.config.locations"]) //println ('* Adding angular-scaffolding configuration files to grails.config.locations');
 	// Get the application's Config.groovy file
-	def cfg = new File("${basedir}/grails-app/conf/Config.groovy");
+	def cfg = new File("${basedir}/grails-app/conf/Config.groovy")
 	def cfgText = cfg.text
 	def appendedText = new StringWriter()
-	appendedText.println ""
-	appendedText.println ("// Added by angular-scaffolding plugin");
+	
 	// Slurp the configuration so we can look at grails.config.locations.
 	def config = new ConfigSlurper().parse(cfg.toURL());
 	// If it isn't defined, create it as a list.
 	if (config.grails.config.locations.getClass() == groovy.util.ConfigObject) {
-	    appendedText.println('grails.config.locations = []');
+		appendedText.println ""
+		appendedText.println ("// Added by angular-scaffolding plugin")
+	    appendedText.println('grails.config.locations = []')
 	} else {
-	    // Don't add configuration files that are already on the list.
+		// Don't add configuration files that are already on the list.
 	    newConfigFiles = newConfigFiles.grep {
 	      !config.grails.config.locations.contains(it)
 	    };
+		
+		if(newConfigFiles) {
+			appendedText.println ""
+			appendedText.println ("// Added by angular-scaffolding plugin")
+		}
 	}
 	// Add each surviving location to the list.
 	newConfigFiles.each {
 	    // The name will have quotes around it...
-	    appendedText.println "grails.config.locations << \"$it\"";
+	    appendedText.println "grails.config.locations << \"$it\""
 	}
 	// Write the new configuration code to the end of Config.groovy.
-	cfg.append(appendedText.toString());
+	cfg.append(appendedText.toString())
 }
 
 eventCompileEnd = {
