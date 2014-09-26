730c146c4934e6c03ca0ef71254322904bb5190e
Alessandro Berbenni <aberbenni@Mac-mini-di-Alessandro.local>
2014-09-26 17:49:12 +0200

    Cleanup

--- grails-app/conf/AngularScaffoldingResources.groovy
+++ grails-app/conf/AngularScaffoldingResources.groovy
@@ -1,23 +1,5 @@
-import grails.util.GrailsUtil
-
-def fileVersion = GrailsUtil.isDevelopmentEnv() ? '1.2.23' : '1.2.23.min'
-
 modules = {
-    /*angular {
-        resource id: 'js', url: [plugin: 'angular-scaffolding', dir: 'js/angular', file: "angular-${fileVersion}.js"], nominify: true, disposition: 'head'
-    }
-
-    'angular-resource' {
-		dependsOn 'angular'
-        resource id: 'js', url: [plugin: 'angular-scaffolding', dir: 'js/angular', file: "angular-resource-${fileVersion}.js"], nominify: true
-    }
-	
-    'angular-route' {
-		dependsOn 'angular'
-        resource id: 'js', url: [plugin: 'angular-scaffolding', dir: 'js/angular', file: "angular-route-${fileVersion}.js"], nominify: true
-    }*/
-	
-    'angular-scaffolding' {
+	'angular-scaffolding' {
         dependsOn 'angular-resource', 'angular-route'
         resource id: 'js', url: [plugin: 'angular-scaffolding', dir: 'js', file: 'scaffolding.js']
         resource id: 'css', url: [plugin: 'angular-scaffolding', dir: 'css', file: 'scaffolding.css']
