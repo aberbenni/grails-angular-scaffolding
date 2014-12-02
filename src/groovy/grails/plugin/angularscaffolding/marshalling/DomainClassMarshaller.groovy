a74462e11fd1b620332eeb75f7e7c9214ec211bf
Alessandro Berbenni <aberbenni@Mac-mini-di-Alessandro.local>
2014-12-02 10:33:45 +0100

    Domain classes marshalling

--- src/groovy/grails/plugin/angularscaffolding/marshalling/DomainClassMarshaller.groovy
+++ src/groovy/grails/plugin/angularscaffolding/marshalling/DomainClassMarshaller.groovy
@@ -2,47 +2,6 @@ package grails.plugin.angularscaffolding.marshalling
  
 import org.codehaus.groovy.grails.commons.DefaultGrailsDomainClass
  
-/**
- * This class provides the inclusion and exclusion policy
- * for Marshallers.
- * 
- * Usage for exclusion policy:
- * <code>
- * JSON.registerObjectMarshaller(MyDomainClass, DomainClassMarshaller.createExcludeMarshaller(MyDomainClass, ["excluedField1", "excluedField2"]))
- * </code>
- * 
- * Usage for inclusion policy:
- * <code>
- * JSON.registerObjectMarshaller(MyDomainClass, DomainClassMarshaller.createIncludeMarshaller(MyDomainClass, ["id", "name", "description"]))
- * </code>
- * 
- * Usage in controller:
- * <code>
- * def sampleAction(Long id) {
- *   def item = Product.getById(id)
- *   response item as JSON
- * }
- * </code>
- * 
- * Create custom configuration:
- * <code>
- * JSON.createNamedConfig("forAdmin") {
- *   JSON.registerObjectMarshaller(MyDomainClass, DomainClassMarshaller.createIncludeMarshaller(MyDomainClass, ["id", "name", "description", "stats"]))
- * }
- * </code>
- * And controller:
- * <code>
- * def sampleAction(Long id) {
- * 	 def item = Product.getById(id)
- *   JSON.use("forAdmin") {
- *     response item as JSON
- *   }
- * }
- * </code>
- * 
- * @author Piotr 'Athlan' Pelczar
- *
- */
 class DomainClassMarshaller {
  
 	public static List<String> globalRestrictedFields = ['class']
@@ -51,10 +10,19 @@ class DomainClassMarshaller {
 		return { domainItem ->
 			DefaultGrailsDomainClass domain = new DefaultGrailsDomainClass(clazz)
 			def results = [:]
-			domain.persistentProperties.each { field ->
-				if(!(field.name in globalRestrictedFields) && (field.name in fieldsToInclude))
-					results[field.name] = domainItem[field.name]
+			
+			def persistentPropNames = domain.persistentProperties*.name
+			
+			if(fieldsToInclude.contains('id'))
+				results[domain.identifier.name] = domainItem[domain.identifier.name]
+			
+			persistentPropNames.each { field ->
+				if(!(field in globalRestrictedFields) && (field in fieldsToInclude))
+					results[field] = domainItem[field]
 			}
+			
+			if(fieldsToInclude.contains('version'))
+				results['version'] = domainItem['version']
 			
 			if(fieldsToInclude.contains('toString'))
 				results['desc'] = domainItem.toString()
@@ -66,12 +34,21 @@ class DomainClassMarshaller {
 	public static Closure createExcludeMarshaller(Class clazz, List<String> fieldsToExclude = []) {
 		return { domainItem ->
 			DefaultGrailsDomainClass domain = new DefaultGrailsDomainClass(clazz)
- 
-			def results = [:]
-			domain.persistentProperties.each { field ->
-				if(!(field.name in globalRestrictedFields) && !(field.name in fieldsToExclude))
-					results[field.name] = domainItem[field.name]
+ 			def results = [:]
+			
+			def persistentPropNames = domain.persistentProperties*.name
+			
+			if(!fieldsToExclude.contains('id'))
+				results[domain.identifier.name] = domainItem[domain.identifier.name]
+			
+			persistentPropNames.each { field ->
+				if(!(field in globalRestrictedFields) && !(field in fieldsToExclude))
+					results[field] = domainItem[field]
 			}
+			
+			if(!fieldsToExclude.contains('version'))
+				results['version'] = domainItem['version']
+			
 			return results
 		}
 	}
